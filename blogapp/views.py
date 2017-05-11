823133dc6864b130b405f96b1bfc79661ee9638f
Germanas Latvaitis <germanas@Germanass-MacBook-Pro.local>
2017-05-11 21:49:51 +0300

    added urls files

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.conf.urls import url
+from django.conf.urls import include
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blogapp/post_list.html', {})
