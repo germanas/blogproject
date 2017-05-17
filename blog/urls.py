679ceca72c5b5d59d06e7e7c1d0dfe1c1092aca1
germanas <germanas.latvaitis@gmail.com>
2017-05-17 22:01:57 +0300

    Added robots

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blogapp.urls')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='blogapp/robots.txt')),
 ]
