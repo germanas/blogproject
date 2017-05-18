66fdf04d45b835d610eb77a35f0ac93aafef26a7
germanas <germanas.latvaitis@gmail.com>
2017-05-18 18:07:12 +0300

    meta view port added

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,4 +1,4 @@
-
+import re
 from django.db import models
 from django.utils import timezone
 from ckeditor.fields import RichTextField
@@ -7,11 +7,14 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = RichTextField()
-    tags = models.ArrayField()
+    tags = models.CharField(default='default_tag',max_length=200)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+
+    def get_tag_list(self):
+        return re.split(" ", self.tags)
 
     def publish(self):
         self.published_date = timezone.now()
